123cd4b604dbddb72a76d8ac24f63d2f65a38181
langfod <langfod@users.noreply.github.com>
2026-06-05 03:19:06 -0400

    revert async spd ; minor syntax fixes

--- dll-source/src/SkyrimNetBridge.h
+++ dll-source/src/SkyrimNetBridge.h
@@ -2,7 +2,7 @@
 
 #include "SkyrimNet_PublicAPI.h"
 
-// Bridges SNBaka_UI.dll to SkyrimNet's native C++ API (SkyrimNet_PublicAPI.h).
+// Bridges SkyrimNet_BakaIntegration.dll to SkyrimNet's native C++ API (SkyrimNet_PublicAPI.h).
 // Resolves the API and registers our action categories. Call Init() once, at
 // SKSE kDataLoaded (the API doc requires resolution there; action/category
 // registration works immediately after).
